ded1c1401e18088c93b2e43cbcd72363cb9c318e
Guilherme <guilhermepontifice.gp@gmail.com>
2025-03-03 14:23:24 -0300

    Adicionando Inputs e Estilos

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss'
 })
 export class RolesComponent {
-//string, number, boolean, date, object, array, null, undefined
+  //string, number, boolean, date, object, array, null, undefined
+  firstName: string = "Tutorial de Angular";
+  angularVersion = "Version 18"
+  version: number = 18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = "button";
+  selectedState: string = "";
+
+  showWelcomeAlert () {
+   alert("Tá Funcionando")  
+  }
+    showMessage(message: string) {
+      alert(message)
+  }
 }

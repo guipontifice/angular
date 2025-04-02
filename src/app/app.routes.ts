356904ee7c93890b1436fe45d5b28b8bdda86a97
Guilherme <guilhermepontifice.gp@gmail.com>
2025-04-02 19:37:15 -0300

    Rotas, Login e Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { MasterComponent } from './components/master/master.component';
 import { ClientComponent } from './components/client/client.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { ClientProjectComponent } from './components/client-project/client-project.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 export const routes: Routes = [
     {
         path: '',
@@ -10,19 +12,29 @@ export const routes: Routes = [
         pathMatch: 'full'
     },
     {
-        path: 'master',
-        component: MasterComponent
+        path: 'login',
+        component: LoginComponent
     },
     {
-        path: 'employee',
-        component: EmployeeComponent
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'master',
+                component: MasterComponent
+            },
+            {
+                path: 'employee',
+                component: EmployeeComponent
+            },
+            {
+                path: 'client',
+                component: ClientComponent
+            },
+            {
+                path: 'client-project',
+                component: ClientProjectComponent
+            }
+        ]
     },
-    {
-        path: 'client',
-        component: ClientComponent
-    },
-    {
-        path: 'client-project',
-        component: ClientProjectComponent
-    }
 ];
